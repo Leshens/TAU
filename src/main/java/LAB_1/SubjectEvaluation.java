5912bebddf4ba9ae56f9e6d55ec3fe915012c68f
Leshens <kacper.kamm@gmail.com>
2023-03-08 18:54:18 +0100

    LAB_1 done

--- src/main/java/LAB_1/SubjectEvaluation.java
+++ src/main/java/LAB_1/SubjectEvaluation.java
@@ -1,4 +1,9 @@
 package LAB_1;
+
+import org.apache.commons.validator.routines.DoubleValidator;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.validator.routines.UrlValidator;
+
 public class SubjectEvaluation {
     public static void main(String[] args) {
         if (args.length<=0){
@@ -16,6 +21,11 @@ public class SubjectEvaluation {
     public static double evaluate(double ... percentages){
             double overallPercentage = sum(percentages)/percentages.length;
 
+            for (double percentage:percentages){
+                if (percentage < 0){
+                    return 0;
+                }
+            }
             if (percentages.length == 0){
                 return 0;
             }
@@ -25,7 +35,34 @@ public class SubjectEvaluation {
                 return 3.0 + 0.5 * (int)((overallPercentage - 0.5)/0.1);
             }
         }
-
+    public static boolean isValidEmail(String emailAddress){
+        EmailValidator validator = EmailValidator.getInstance();
+        return validator.isValid(emailAddress);
+    }
+    public static String isValidUrl(String url){
+        UrlValidator urlValidator = UrlValidator.getInstance();
+        if (urlValidator.isValid(url)) {
+            return "url is valid";
+        } else {
+            return "url is invalid";
+        }
+    }
+    public static String isUrlHttps(String url){
+        String[] schemes = {"http","https"};
+        UrlValidator urlValidator = new UrlValidator(schemes);
+        if (urlValidator.isValid(url)) {
+            return "url is valid";
+        } else {
+            return "url is invalid";
+        }
+    }
+    public static String isDouble(String value){
+        DoubleValidator doubleValidator = DoubleValidator.getInstance();
+        if (doubleValidator.isValid(value)){
+            return value;
+        }else
+            return null;
+    }
         private static double sum(double[] percentages) {
             double sum=0;
             for (double percentage:percentages){
